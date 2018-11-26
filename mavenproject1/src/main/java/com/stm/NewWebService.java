da0c83598ffae49270079a57e94dff086a44106f
Michal <michalwir@gmail.com>
2018-11-26 21:41:54 +0100

    working server and firs screen of android app

--- mavenproject1/src/main/java/com/stm/NewWebService.java
+++ mavenproject1/src/main/java/com/stm/NewWebService.java
@@ -5,6 +5,12 @@
  */
 package com.stm;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,6 +19,7 @@ import java.net.URISyntaxException;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -23,7 +30,13 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "NewWebService")
 public class NewWebService {
-
+    
+    public final float  longtitudeMAX = 51.4167f;
+    public final float longtitudeMIN =  51.40892f;
+    public final float  longtitudeSize = longtitudeMAX-longtitudeMIN;
+    public final float latitudeMAX = 21.15f;
+    public final float latitudeMIN = 21.136117f;
+    public final float latitudeSize = latitudeMAX-latitudeMIN;
     /**
      * This is a sample web service operation
      */
@@ -59,5 +72,61 @@ public class NewWebService {
 		System.out.println("Exception while reading the Image " + ioe);
 	}
 	return base64Image;
+    }
+    
+    /**
+     *
+     * @param longitude
+     * @param latitude
+     * @return
+     */
+    @WebMethod(operationName = "getMap")
+    public String getMap(@WebParam(name = "longitude1")  Float longitude1, @WebParam(name = "latitude1") Float latitude1,
+            @WebParam(name = "longitude2")  Float longitude2, @WebParam(name = "latitude2") Float latitude2) {
+       String base64Image = "";
+       /*TODO zabezpieczenia*/
+       
+       
+	ClassLoader classLoader = getClass().getClassLoader();  
+        BufferedImage cuttedImage = null;
+        byte[] imageInByte = null;
+        try {
+      System.out.println("Value = " + classLoader.getResource("mapa.PNG").toURI());
+            File file = new File(classLoader.getResource("mapa.PNG").toURI());
+
+            Image srcImg = ImageIO.read(file);
+            cuttedImage = cutImage(srcImg, longitude1, latitude1, longitude2, latitude2); 
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write( cuttedImage, "PNG", baos );
+            baos.flush();
+            imageInByte = baos.toByteArray();
+            base64Image = Base64.getEncoder().encodeToString(imageInByte); 
+            baos.close();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(NewWebService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(NewWebService.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+        
+	return base64Image;
+    }
+    
+    
+    private BufferedImage cutImage(Image src, float longitude1, float latitude1, float longitude2, float latitude2) throws IOException{
+        int x = Math.round(((longitude1-longtitudeMIN)/longtitudeSize)*1000f); 
+        int y = Math.round(((latitude2-latitudeMIN)/latitudeSize)*1000f);  
+        int w = Math.abs(Math.round(((longitude2-longitude1)/longtitudeSize)*1000f)); 
+        int h =  Math.abs(Math.round(((latitude2-latitude1)/latitudeSize)*1000f));
+        w=Math.abs(w);
+        h=Math.abs(h);
+        BufferedImage dest = ((BufferedImage)src).getSubimage(x, y, w, h);
+//        BufferedImage dst = new BufferedImage(w, h, BufferedImage.TYPE_INT_BGR);
+//        Graphics g = dst.getGraphics();
+//        g.drawImage(src, 0, 0, w, h, x, y, x + w, y + h, null);
+//             BufferedImage bi = (BufferedImage)src;
+        ImageIO.write(dest, "png", new File("duke_croppedaaa.png"));
+//        ImageIO.write(src, "png", new File("duke_cropped.png"));
+        return dest;
+        
     }
 }
